27c6495daad539804325dd0f996464aaf85a34ce
root <mudassirkhan19@gmail.com>
2023-03-21 11:08:41 +0000

    add download sd script, modify requirements

---
+++ bin/download_sd14.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from huggingface_hub import snapshot_download
+from pathlib import Path
+
+models = Path('models')
+sd1_4 = models / 'sd1_4'
+sd1_4.mkdir(exist_ok=True, parents=True)
+snapshot_download("CompVis/stable-diffusion-v1-4", repo_type="model", local_dir=sd1_4)
